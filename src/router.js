faee1c2d35e4f3475972efc0d4d526dc8d8c784c
MarcosVel <marcos.veloso@sempreceub.com>
2022-03-04 00:25:14 -0300

    cleaning project and add routes

---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/repository/:repositorio' element={<Repository />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
