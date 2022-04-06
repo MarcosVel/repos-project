5c233233b8a6fe4a01e2a3b879f4b44089b5b0f4
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-06 19:04:25 -0300

    getting from api the repo and issues

--- src/pages/Repository/index.jsx
+++ src/pages/Repository/index.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Container } from "./styles";
+import api from "../../services/api";
 
 function Repository() {
   const { repositorio } = useParams();
 
+  useEffect(() => {
+    async function load() {
+      const [repoData, issuesData] = await Promise.all([
+        api.get(`/repos/${repositorio}`),
+        api.get(`/repos/${repositorio}/issues`)
+      ]);
+    }
+
+    load();
+  }, [])
+
   return (
-    <>
-      <h1 style={{ color: "white" }}>
+      <Container style={{ color: "white" }}>
         {repositorio}
-      </h1>
-    </>
+      </Container>
   );
 }
 
