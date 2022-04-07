bdbb23145c6ec5570f106ed0bd4adeeb2376aa72
MarcosVel <marcos.veloso@sempreceub.com>
2022-04-07 20:27:41 -0300

    add loading animation

--- src/pages/Repository/index.jsx
+++ src/pages/Repository/index.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container } from "./styles";
+import { Container, Loading, Owner } from "./styles";
 import api from "../../services/api";
+import { FaSpinner } from "react-icons/fa";
 
 function Repository() {
   const { repositorio } = useParams();
-  const [ repo, setRepo ] = useState({});
-  const [ repoIssues, setRepoIssues ] = useState([]);
-  const [ loading, setLoading ] = useState(true);
+  const [repo, setRepo] = useState({});
+  const [repoIssues, setRepoIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function load() {
@@ -16,9 +17,9 @@ function Repository() {
         api.get(`/repos/${repositorio}/issues`, {
           params: {
             state: "open",
-            per_page: 5
-          }
-        })
+            per_page: 5,
+          },
+        }),
       ]);
       // console.log(repoData.data);
       // console.log(issuesData.data);
@@ -26,14 +27,28 @@ function Repository() {
       setRepoIssues(issuesData.data);
       setLoading(false);
     }
-    
+
     load();
   }, [repositorio]);
 
+  console.log(repo);
+
+  if (loading) {
+    return (
+      <Loading>
+        <FaSpinner color="#fff" size={48} />
+      </Loading>
+    );
+  }
+
   return (
-      <Container style={{ color: "white" }}>
-        {repositorio}
-      </Container>
+    <Container style={{ color: "white" }}>
+      <Owner>
+        <img src={repo?.owner?.avatar_url} alt={repo?.owner?.login} />
+        <h1>{repo?.name}</h1>
+        <p>{repo?.description}</p>
+      </Owner>
+    </Container>
   );
 }
 
--- src/pages/Repository/styles.js
+++ src/pages/Repository/styles.js
@@ -1,5 +1,28 @@
-import styled from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
-export const Container = styled.div`
-  
+export const Container = styled.div``;
+
+export const Owner = styled.header``;
+
+const animate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+
+  ${css`
+    svg {
+      animation: ${animate} 2s linear infinite;
+    }
+  `}
 `;
